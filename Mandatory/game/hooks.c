ab8a8760b4898770c00b06f31210d0c1db8e63a6
Trafexofive <les5phenix@gmail.com>
2023-11-15 06:01:07 +0100

    fast commit

--- Mandatory/game/hooks.c
+++ Mandatory/game/hooks.c
@@ -6,7 +6,7 @@
 /*   By: mlamkadm <mlamkadm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 15:01:01 by mlamkadm          #+#    #+#             */
-/*   Updated: 2023/11/15 05:45:33 by mlamkadm         ###   ########.fr       */
+/*   Updated: 2023/11/15 06:01:00 by mlamkadm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	key_hook(int key, t_game_info *game)
 {
-	if ((key == W_KEY) && 
-		check_collision(game->map[game->p_pos.y - 1][game->p_pos.x], game))
-		player_move(game,0,-1);
-	else if ((key == S_KEY) && 
-		check_collision(game->map[game->p_pos.y + 1][game->p_pos.x], game))
-		player_move(game,0,1);
-	else if ((key == A_KEY) &&  
-		check_collision(game->map[game->p_pos.y][game->p_pos.x - 1], game))
-		player_move(game,-1,0);
-	else if ((key == D_KEY) &&  
-		check_collision(game->map[game->p_pos.y][game->p_pos.x + 1], game))
-		player_move(game,+1,0);
+	if (key == W_KEY && check_collision
+		(game->map[game->p_pos.y - 1][game->p_pos.x], game))
+		player_move(game, 0, -1);
+	else if (key == S_KEY && check_collision
+		(game->map[game->p_pos.y + 1][game->p_pos.x], game))
+		player_move(game, 0, 1);
+	else if (key == A_KEY && check_collision
+		(game->map[game->p_pos.y][game->p_pos.x - 1], game))
+		player_move(game, -1, 0);
+	else if (key == D_KEY && check_collision
+		(game->map[game->p_pos.y][game->p_pos.x + 1], game))
+		player_move(game, +1, 0);
 	else if (key == ESC_KEY)
 		free_all(game);
 }
@@ -39,18 +39,17 @@ int	check_collision(char c, t_game_info *game)
 	return (TRUE);
 }
 
-int  colision(char c)
+int	colision(char c)
 {
 	if (c == '1' || c == 'E')
 		return (1);
 	else if (c == 'C')
 		return (3);
 	else
-		return(0);
+		return (0);
 }
 
-
-void player_move(t_game_info *game , int x, int y) 
+void	player_move(t_game_info *game, int x, int y)
 {
 	if (game->map[game->p_pos.y + y][game->p_pos.x + x] == 'C' )
 	{
@@ -58,8 +57,9 @@ void player_move(t_game_info *game , int x, int y)
 		game->exit_allowed = (game->c_count == 0);
 		game->map[game->p_pos.y + y][game->p_pos.x + x] = '0';
 	}
-	if (game->map[game->p_pos.y + y][game->p_pos.x + x] == 'E' && game->exit_allowed == TRUE)
-		ft_exit("Win Mutha Sucka",2);
+	if (game->map[game->p_pos.y + y][game->p_pos.x + x] == 'E'
+		&& game->exit_allowed == TRUE)
+		free_exit(game);
 	game->p_pos.x += x;
 	game->p_pos.y += y;
 	ft_putnbr_fd(game->move_count++, 1);
